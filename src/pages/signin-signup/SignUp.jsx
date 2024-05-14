d9c6442c97feb7589bb3eed13cf9525ccf49efad
Sujan <mail.thapasujan@gmail.com>
2024-05-14 16:14:58 +1000

    signup functionality implemented

--- src/pages/signin-signup/SignUp.jsx
+++ src/pages/signin-signup/SignUp.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { DefaultLayout } from "../../components/layout/DefaultLayout";
 import { Button, Col, Form, Row } from "react-bootstrap";
 import { CustomInput } from "../../components/customInput/CustomInput";
+import { toast } from "react-toastify";
+import { postNewUser } from "../../helpers/axiosHelper";
 
 const SignUp = () => {
+  const [form, setForm] = useState({});
+  const [error, setError] = useState("");
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setForm({ ...form, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const { cpassword, ...rest } = form;
+    if (cpassword !== rest.password) {
+      return alert("Passwords do not match");
+    }
+    const responsePending = postNewUser(rest);
+    toast.promise(responsePending, { pending: "Creating your User....." });
+    const { status, message } = await responsePending;
+    toast[status](message);
+  };
+
   const inputs = [
     { label: "First Name", name: "fname", type: "text", required: true, placeholder: "First Name" },
     { label: "Last Name", name: "lname", type: "text", required: true, placeholder: "Last Name" },
@@ -30,21 +54,33 @@ const SignUp = () => {
       placeholder: "Confirm Password",
     },
   ];
+
   return (
     <DefaultLayout>
       <Row>
         <Col>
           <Form
+            onSubmit={handleSubmit}
             className="shadow-lg border border-secondary p-5 rounded m-auto mt-4"
             style={{ width: "450px" }}
           >
             <h1>Join the Book Store Community!!!!!</h1>
             <hr />
             {inputs.map((input, i) => (
-              <CustomInput key={i} {...input} />
+              <CustomInput key={i} {...input} onChange={handleChange} />
             ))}
+            {error && (
+              <div className="my-3">
+                <ul>
+                  <li className="text-danger fw-bolder">{error}</li>
+                </ul>
+              </div>
+            )}
+
             <div className="d-grid">
-              <Button type="submit">Submit</Button>
+              <Button type="submit" disabled={error}>
+                Submit
+              </Button>
             </div>
           </Form>
         </Col>
