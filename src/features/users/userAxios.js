d4209d974d1b82e02df22cc227e25b1193933f3f
Sujan <mail.thapasujan@gmail.com>
2024-05-21 12:15:51 +1000

    Saving unsaved changes

---
+++ src/features/users/userAxios.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+import { apiProcessor } from "../../helpers/axiosHelper";
+
+const rootAPI = import.meta.env.VITE_APP_ROOTAPI;
+const userEp = rootAPI + "/users";
+
+
+export const loginUser = async (userObj) => {
+    const axiosObj = { method: 'POST', url: userEp + "/login", data: userObj }
+    return await apiProcessor(axiosObj);
+};
+
+export const postNewUser = async (userObj) => {
+    const axiosObj = { method: 'POST', url: userEp, data: userObj }
+    return await apiProcessor(axiosObj);
+};
