8400e46195cdb6f517fa9d95d33281742f6d31bf
thapaji <mail.thapasujan@gmail.com>
2024-10-17 13:22:00 +0930

    stock shown in book landing

--- src/pages/book/BookLanding.jsx
+++ src/pages/book/BookLanding.jsx
@@ -17,22 +17,22 @@ const BookLanding = () => {
   const [averageRating, setAverageRating] = useState(0);
 
   useEffect(() => {
-    if (book && book.reviews) {
+    if (book?.reviews?.length) {
       const totalRating = book.reviews.reduce((acc, review) => acc + review.ratings, 0);
-      const avg = book.reviews.length > 0 ? totalRating / book.reviews.length : 0;
+      const avg = totalRating / book.reviews.length;
       setAverageRating(avg);
     }
   }, [book]);
 
   const handleBookBorrow = () => {
     if (window.confirm("Are you sure you want to borrow this book?")) {
-      dispatch(postNewBorrowAction({ bookId: book._id, bookTitle: book?.title, thumbnail: book.thumbnail }));
+      dispatch(postNewBorrowAction({ bookId: book._id, bookTitle: book.title, thumbnail: book.thumbnail }));
     }
   };
 
+  // Stock calculations
   const isOutOfStock = book?.count <= book?.borrowed;
   const remainingCopies = book?.count - book?.borrowed;
-  const isLastCopy = remainingCopies === 1;
 
   return (
     <DefaultLayout pageTitle={"Book: " + book?.title}>
@@ -41,27 +41,29 @@ const BookLanding = () => {
           <Col md={6}>
             <img src={book?.thumbnail} alt="book" style={{ width: "70%" }} />
           </Col>
-          <Col md={6} className="col">
+          <Col md={6}>
             <Row>
               <p>
                 {book?.author} - {book?.publishedYear}
               </p>
               <Stars stars={averageRating} readOnly={true} />
               <p>{book?.description.slice(0, 50)}...</p>
+              <p>Remaining Copies: {remainingCopies}</p> {/* Display remaining stock */}
               {user?._id ? (
-                isOutOfStock ? (
-                  <p className="text-danger">Out of Stock</p>
-                ) : (
-                  <Button variant="warning" onClick={handleBookBorrow}>
-                    {isLastCopy ? "Get Yours Now!" : "Borrow Book Now"}
-                  </Button>
-                )
+                <Button
+                  variant="warning"
+                  onClick={handleBookBorrow}
+                  disabled={isOutOfStock} // Disable if out of stock
+                >
+                  {isOutOfStock ? "Out of Stock" : "Borrow Book Now"}
+                </Button>
               ) : (
                 <Link to="/signin" state={{ from: { location } }}>
                   <Button variant="warning">Login to borrow</Button>
                 </Link>
               )}
             </Row>
+
             <Row className="mt-5">
               <Tabs defaultActiveKey="description">
                 <Tab eventKey="description" title="Description">
